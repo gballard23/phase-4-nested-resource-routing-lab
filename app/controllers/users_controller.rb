26d971ffe6ebf029d7526fb1eec740a4148ae441
Gerren <g.ballard23@outlook.com>
2022-10-05 08:53:13 -0500

    commit tree 3610a76dcb829e7c84250b50a2536cee3cd5bd54

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index 
+    user = User.all 
+    render json: user, include: :items 
+  end 
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
